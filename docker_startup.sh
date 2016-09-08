c715feedd868410087b5253318196a75a18bfb35
Ed Schouten <ed@kumina.nl>
2016-09-08 16:02:35 +0200

    Sync in from the proper subdirectory in the bucket.

--- docker_startup.sh
+++ docker_startup.sh
@@ -2,11 +2,13 @@
 
 set -e
 
+export LOCAL_PATH=/srv/data/
+
 # Perform the initial sync from S3 to the local system.
-aws s3 sync --exclude .volume_ready "${S3_PATH}" /srv/data
+aws s3 sync --exclude .volume_ready "${S3_PATH}${LOCAL_PATH}" "${LOCAL_PATH}"
 
 # Put a marker in place indicating that we've started up successfully.
 touch /srv/data/.volume_ready
 
 # Start up the AWS syncer to sync back from the local system to S3.
-LOCAL_PATH=/srv/data/ exec awssyncer
+exec awssyncer
