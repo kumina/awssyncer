8e3b296d01bba809bb9242234ceb72f95ae375b8
Ed Schouten <ed@kumina.nl>
2016-09-08 13:32:47 +0200

    Clean up the AWS syncer's main loop.

--- src/awssyncer.cc
+++ src/awssyncer.cc
@@ -9,16 +9,35 @@
 
 #include <cassert>
 #include <chrono>
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 #include <thread>
 
-int main() {
+// Returns a reference to the logging sink, already prefixing the line with the
+// current timestamp.
+static std::ostream& Log() {
+  time_t t = std::time(nullptr);
+  char buf[32];
+  std::strftime(buf, sizeof(buf), "%FT%T%z: ", std::localtime(&t));
+  return std::cout << buf;
+}
+
+// Performs a single iteration of the AWS syncer, terminating if an error has
+// occurred.
+static void RunOnce(const std::string& local_path, const std::string& s3_path) {
+  // Set up inotify polling.
   NonrecursiveInotifyPoller nip;
   RecursiveInotifyPoller rip(&nip);
-  bool res = rip.AddWatch("/home/ed/syncdir/");
+  bool res = rip.AddWatch(local_path);
   assert(res);
 
+  // Start off marking the entire file system as dirty. This forces an initial
+  // synchronisation of all of the data.
   FilesystemDirt dirt;
+  dirt.AddDirtyPath(local_path);
+
+  // Objects for running the AWS command line tool sequentially.
   PosixCommandRunner posix_runner;
   MultipleCommandRunner multiple_command_runner(&posix_runner);
   AwsCommandRunner runner(&multiple_command_runner, "s3://oreokoekje");
@@ -29,19 +48,19 @@ int main() {
     while (rip.GetNextEvent(&event))
       dirt.AddDirtyPath(event.path);
     if (nip.EventsDropped()) {
-      // TODO(ed): Deal with this.
-      std::cout << "DROPPED EVENTS" << std::endl;
+      Log() << "Inotify dropped events" << std::endl;
+      return;
     }
 
     // Potentially spawn another sync command.
     if (runner.Finished()) {
       if (runner.PreviousCommandFailed()) {
-        // TODO(ed): Deal with this.
-        std::cout << "PREVIOUS COMMAND FAILED" << std::endl;
+        Log() << "Failed to execute command" << std::endl;
+        return;
       }
       if (dirt.HasDirtyPaths()) {
         std::string path = dirt.ExtractDirtyPath();
-        std::cout << "PROCESSING PATH " << path << std::endl;
+        Log() << "Processing path " << path << std::endl;
         struct stat sb;
         if (lstat(path.c_str(), &sb) == 0) {
           if (S_ISDIR(sb.st_mode))
@@ -51,14 +70,28 @@ int main() {
         } else if (errno == ENOENT) {
           runner.Remove(path);
         } else {
-          // TODO(ed): Deal with this.
-          std::cout << "FAILED TO STAT" << path << std::endl;
+          Log() << "Failed to stat " << path << std::endl;
         }
       }
     }
 
     // TODO(ed): Call poll() here instead of sleeping for a second.
-    std::cout << "End quantum" << std::endl;
+    Log() << "Next iteration in one second" << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
+}
+
+int main() {
+  // Obtain configuration from environment variables.
+  std::string local_path = std::getenv("WATCH_DIR");
+  std::string s3_path = std::getenv("BUCKET");
+
+  // Keep on running indefinitely, restarting if an error occurred. Put a pause
+  // of a couple of seconds in between, so that we never perform any actions in
+  // a tight loop.
+  for (;;) {
+    RunOnce(local_path, s3_path);
+    Log() << "Restarting in five seconds" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(5));
   }
 }
