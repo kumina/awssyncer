45a82074d8f2f32238c4a24b351770afd006cdb4
Ed Schouten <ed@kumina.nl>
2016-09-08 12:02:21 +0200

    Add a queue for running multiple commands.

---
+++ src/multiple_command_runner.cc
@@ -0,0 +1,35 @@
+#include "command_runner.h"
+#include "multiple_command_runner.h"
+
+#include <cassert>
+
+bool MultipleCommandRunner::Finished() {
+  if (!command_runner_->Finished())
+    return false;
+
+  if (!enqueued_commands_.empty()) {
+    PreviousCommandFailed();
+    const auto& next = enqueued_commands_.front();
+    command_runner_->RunCommand(next.first);
+    ignore_failures_for_current_command_ = next.second;
+    enqueued_commands_.pop();
+    return false;
+  }
+
+  return true;
+}
+
+bool MultipleCommandRunner::PreviousCommandFailed() {
+  if (previous_command_failed_)
+    return true;
+  if (!ignore_failures_for_current_command_)
+    previous_command_failed_ = command_runner_->PreviousCommandFailed();
+  return previous_command_failed_;
+}
+
+void MultipleCommandRunner::RunCommand(const std::vector<std::string> &command,
+                                       bool ignore_failure) {
+  assert(!previous_command_failed_ &&
+         "Attempted to schedule more commands after failure");
+  enqueued_commands_.push({command, ignore_failure});
+}
---
+++ src/multiple_command_runner.h
@@ -0,0 +1,35 @@
+#ifndef MULTIPLE_COMMAND_RUNNER_H
+#define MULTIPLE_COMMAND_RUNNER_H
+
+#include <queue>
+#include <string>
+#include <vector>
+
+class CommandRunner;
+
+class MultipleCommandRunner {
+ public:
+  MultipleCommandRunner(CommandRunner *command_runner)
+      : command_runner_(command_runner),
+        ignore_failures_for_current_command_(false),
+        previous_command_failed_(false) {}
+
+  // Returns true of there is no command currently running.
+  bool Finished();
+
+  // Returns true if one of the previous commands did not complete
+  // successfully.
+  bool PreviousCommandFailed();
+
+  // Enqueues a command by specifying a list of command line arguments.
+  virtual void RunCommand(const std::vector<std::string> &command,
+                          bool ignore_failure);
+
+ private:
+  CommandRunner *command_runner_;
+  std::queue<std::pair<std::vector<std::string>, bool>> enqueued_commands_;
+  bool ignore_failures_for_current_command_;
+  bool previous_command_failed_;
+};
+
+#endif
