6777080b9d71e93e1d8975060776038cc7290206
Ed Schouten <ed@kumina.nl>
2016-09-09 11:00:18 +0200

    Switch the FilteringInotifyPoller test over to use Google Mock.
    This makes it a lot more readable.

--- src/filtering_inotify_poller_test.cc
+++ src/filtering_inotify_poller_test.cc
@@ -1,62 +1,63 @@
 #include "filtering_inotify_poller.h"
 #include "inotify_poller.h"
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include <experimental/optional>
 #include <string>
 
+using ::testing::Return;
+
 namespace {
 
-// TODO(ed): Rewrite this to use gmock.h.
-class FixedResponseInotifyPoller : public InotifyPoller {
+class MockInotifyPoller : public InotifyPoller {
  public:
-  FixedResponseInotifyPoller() : done_(false) {
-  }
-
-  bool AddWatch(const std::string& path) override {
-    EXPECT_EQ("/some/other/path", path);
-    return true;
-  }
-
-  std::experimental::optional<InotifyEvent> GetNextEvent() {
-    InotifyEvent event;
-    event.path = done_ ? "/some/other/path" : "/matching/path/";
-    done_ = true;
-    return event;
-  }
-
-  bool EventsDropped() {
-    return false;
-  }
-
- private:
-  bool done_;
+  MOCK_METHOD1(AddWatch, bool(const std::string& path));
+  MOCK_METHOD0(GetNextEvent, std::experimental::optional<InotifyEvent>());
+  MOCK_METHOD0(EventsDropped, bool());
 };
+
 }
 
 TEST(FilteringInotifyPollerTest, AddWatch) {
   // We shouldn't be allowed to install watches on matching paths.
-  FixedResponseInotifyPoller frip;
-  FilteringInotifyPoller fip(&frip, "matching");
+  MockInotifyPoller mip;
+  EXPECT_CALL(mip, AddWatch("/some/other/path/1"))
+      .WillOnce(Return(true));
+  EXPECT_CALL(mip, AddWatch("/some/other/path/2"))
+      .WillOnce(Return(false));
+
+  FilteringInotifyPoller fip(&mip, "matching");
   ASSERT_FALSE(fip.AddWatch("/matching/path/"));
-  ASSERT_TRUE(fip.AddWatch("/some/other/path"));
+  ASSERT_TRUE(fip.AddWatch("/some/other/path/1"));
+  ASSERT_FALSE(fip.AddWatch("/some/other/path/2"));
 }
 
-TEST(FilteringInotifyPollerTest, GetNextEvent_match) {
-  // We should return the second event, as we have matches.
-  FixedResponseInotifyPoller frip;
-  FilteringInotifyPoller fip(&frip, "matching");
+TEST(FilteringInotifyPollerTest, GetNextEvent) {
+  // We should return the second event, as the first one is matched.
+  InotifyEvent event1, event2;
+  event1.path = "/matching/path";
+  event2.path = "/some/other/path";
+  MockInotifyPoller mip;
+  EXPECT_CALL(mip, GetNextEvent())
+      .WillOnce(Return(event1))
+      .WillOnce(Return(event2));
+
+  FilteringInotifyPoller fip(&mip, "matching");
   std::experimental::optional<InotifyEvent> event = fip.GetNextEvent();
   ASSERT_TRUE(event);
   ASSERT_EQ("/some/other/path", event->path);
 }
 
-TEST(FilteringInotifyPollerTest, GetNextEvent_nomatch) {
-  // We should return the first event, as there is no match.
-  FixedResponseInotifyPoller frip;
-  FilteringInotifyPoller fip(&frip, "not_present");
-  std::experimental::optional<InotifyEvent> event = fip.GetNextEvent();
-  ASSERT_TRUE(event);
-  ASSERT_EQ("/matching/path/", event->path);
+TEST(FilteringInotifyPollerTest, EventsDropped) {
+  // Test propagation of EventsDropped() calls.
+  MockInotifyPoller mip;
+  EXPECT_CALL(mip, EventsDropped())
+      .WillOnce(Return(false))
+      .WillOnce(Return(true));
+
+  FilteringInotifyPoller fip(&mip, "matching");
+  ASSERT_FALSE(fip.EventsDropped());
+  ASSERT_TRUE(fip.EventsDropped());
 }
