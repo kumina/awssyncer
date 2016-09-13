843d61cb392b49ebba849ab3f9a9f6ff9b45c90c
Ed Schouten <ed@kumina.nl>
2016-09-13 16:45:40 +0200

    Add missing Copyright statements.

--- src/periodic_filesystem_dirtier.h
+++ src/periodic_filesystem_dirtier.h
@@ -1,3 +1,8 @@
+// Copyright (c) 2016 Kumina, https://kumina.nl/
+//
+// This file is distributed under a 2-clause BSD license.
+// See the LICENSE file for details.
+
 #ifndef PERIODIC_FILESYSTEM_DIRTIER_H
 #define PERIODIC_FILESYSTEM_DIRTIER_H
 
