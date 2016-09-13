ef1f08344b0526e7d2e911d13a06e38ba00c23ef
Ed Schouten <ed@kumina.nl>
2016-09-13 19:59:19 +0200

    Don't hardcode /srv/data.

--- docker_startup.sh
+++ docker_startup.sh
@@ -8,7 +8,7 @@ export LOCAL_PATH=/srv/data/
 aws s3 sync --exclude .volume_ready "s3://${S3_BUCKET}" "${LOCAL_PATH}"
 
 # Put a marker in place indicating that we've started up successfully.
-touch /srv/data/.volume_ready
+touch "${LOCAL_PATH}/.volume_ready"
 
 # Start up the AWS syncer to sync back from the local system to S3.
 exec awssyncer
