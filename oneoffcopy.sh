50a662ad14a19782b77eef66ba6ed33ebefdcdbf
Liam Macgillavry <liam@kumina.nl>
2016-09-14 14:34:28 +0200

    oneoffcopy also has support for s3cmd args now (for --include and --exclude)

--- oneoffcopy.sh
+++ oneoffcopy.sh
@@ -7,4 +7,4 @@ set -e
 export LOCAL_PATH=/srv/data/
 
 # Perform the initial sync from S3 to the local system.
-aws s3 cp "s3://${S3_BUCKET}/${S3_OBJECT}" "${LOCAL_PATH}/"
+aws s3 cp "s3://${S3_BUCKET}/${S3_OBJECT}" "${LOCAL_PATH}/" "${S3_ARGS}"
