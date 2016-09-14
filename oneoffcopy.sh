71335ffbcf32c5d70bbc2e2c399ce56d59fef362
Liam Macgillavry <liam@kumina.nl>
2016-09-14 11:12:16 +0200

    alternative entrypoint oneoffcopy.sh for oneway copy

---
+++ oneoffcopy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# alternative entrypoint for oneshot oneway syncs
+
+set -e
+
+export LOCAL_PATH=/srv/data/
+
+# Perform the initial sync from S3 to the local system.
+aws s3 cp "s3://${S3_BUCKET}${S3_OBJECT}" "${LOCAL_PATH}/${LOCAL_OBJECT}"
